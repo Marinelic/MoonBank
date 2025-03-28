733c15a7a8be4a8ff22eab538eb3d93129b44155
Marinelic <75620592+Marinelic@users.noreply.github.com>
2025-03-28 21:23:37 +0100

    Starting With Hero Section

--- bank_app/src/components/Navbar.jsx
+++ bank_app/src/components/Navbar.jsx
@@ -6,15 +6,13 @@ function Navbar() {
 
   const [toggle, setToggle] = useState(false);
 
-
-
   return (
    <nav className='w-full flex py-6 justify-between items-center navbar'>
       <img src={logo} alt="mabank" className='w-[170px] h-[90px]' />
 
       <ul className='list-none sm:flex hidden justify-end items-center flex-1'>
         {navLinks.map((nav, index) => (
-          <li key={nav.id} className={`font-poppins font-normal cursor-pointer text-[16px] ${index === navLinks.length - 1 ? 'mr-0' : 'mr-10'} text-black`}>
+          <li key={nav.id} className={`font-poppins font-normal cursor-pointer text-[16px] ${index === navLinks.length - 1 ? 'mr-0' : 'mr-10'} text-white`}>
             <a href={`#${nav.id}`}>{nav.title}</a>
           </li>
         ))}
@@ -34,7 +32,6 @@ function Navbar() {
         </div>
       </div>
    </nav>
-   
   )
 }
 
--- bank_app/tailwind.config.js
+++ bank_app/tailwind.config.js
@@ -4,7 +4,7 @@ export default {
   theme: {
     extend: {
       colors: {
-        primary: "#e5f789",
+        primary: "#346684",
         secondary: "#00f6ff",
         dimWhite: "rgba(255, 255, 255, 0.7)",
         dimBlue: "rgba(9, 151, 124, 0.1)",
