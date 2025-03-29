124a6e7be2cd723b94daa8ead24767f82d1df73a
Marinelic <75620592+Marinelic@users.noreply.github.com>
2025-03-29 16:38:25 +0100

    Adding Header

--- bank_app/src/components/Hero.jsx
+++ bank_app/src/components/Hero.jsx
@@ -1,5 +1,6 @@
 import styles from "../style";
 import { discount, robot } from "../assets";
+import GetStarted from './GetStarted';
 
 const Hero = () => (
  <section id="home" className={`flex md:flex-row flex-col ${styles.paddingY}`}>
@@ -15,10 +16,30 @@ const Hero = () => (
 
       <div className="flex flex-row justify-between items-center w-full">
         <h1 className="flex-1 font-poppins font-semibold ss:text-[72px] text-[52px] text-white ss:leading-[100px] leading-[75px]">
-          The Next <br className="sm:block hidden" /> {" "} 
-          <span className="text-gradient">Generation</span> {" "} Payment Method.
-        </h1>
+          The Future of <br className="sm:block hidden" /> {" "} 
+          <span className="text-gradient">Banking</span> {" "} is Here.
+        </h1>  
+
+        <div className="ss:flex md:mr-4 mr-0">
+          <GetStarted />
+        </div>
       </div>
+
+      <h1 className="font-poppins font-semibold ss:text-[68px] text-[52px] text-white ss:leading-[100px] leading-[75px] w-full">
+        Seamless. Secure. Limitless.
+      </h1>
+
+     <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+        Say goodbye to outdated banking. Experience instant transactions, AI-powered insights, and financial freedom—all in one place.
+    </p>
+    </div>
+
+    <div>
+      <img src={robot} alt="billing" className="w-[100%] h-[100%] relative z-[5]" />
+
+      <div className="absolute z-[0] w-[40%] h-[35%] top-0 pink__gradient" />
+        <div className="absolute z-[1] w-[80%] h-[80%] rounded-full white__gradient bottom-40" />
+        <div className="absolute z-[0] w-[50%] h-[50%] right-20 bottom-20 blue__gradient" />
     </div>
  </section>
 )
