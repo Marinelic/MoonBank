80a337870d64d264e68936805d45b733e3334b8f
Marinelic <75620592+Marinelic@users.noreply.github.com>
2025-03-30 20:29:48 +0200

    Adding Section Business

--- bank_app/src/components/Business.jsx
+++ bank_app/src/components/Business.jsx
@@ -1,10 +1,46 @@
-import React from 'react'
+import { features } from "../constants";
+import styles, { layout } from "../style";
+import Button from './Button';
+
+
+const FeatureCard = ({ icon, title, content, index }) => (
+  <div className={`flex flex-row p-6 rounded-[10px] ${index !== features.length - 1 ? 'mb:6' : 'mb-0'} feature-card`}>
+    <div className={`w-[64px] h-[64px] rounded-full ${styles.flexCenter} bg-discount-gradient`}>
+      <img src={icon} alt="icon" className="w-[50%] h-[50%] object-contain"/>
+    </div>
+
+    <div className="flex-1 flex flex-col ml-3">
+      <h4 className="font-poppins font-semibold text-white text-[18px] leading-[23px] mb-1">
+        {title}
+      </h4>
+
+      <p className="font-poppins font-normal text-dimWhite text-[16px] leading-[24px] mb-1">
+        {content}
+      </p>
+    </div>
+  </div>
+)
+
 
 function Business() {
   return (
-    <div>
-      
-    </div>
+    <section id="features" className={layout.section}>
+      <div className={layout.sectionInfo}>
+        <h2 className={styles.heading2}>You focus on the business, <br className="sm:block hidden" /> we’ll take care of the finances.</h2>
+
+        <p className={`${styles.paragraph} max-w-[470px] mt-5 pb-6`}>
+           The right credit card can help you build credit, earn rewards, and save money—but with so many options available, choosing the best one can be challenging.
+        </p>
+
+        <Button styles='mt-10'/>
+      </div>
+
+      <div className={`${styles.sectionImg} flex-col`}>
+        {features.map((feature, index) => (
+         <FeatureCard key={feature.id} {...feature} index={index} />
+        ))}
+      </div>
+    </section>
   )
 }
 
--- bank_app/src/components/Button.jsx
+++ bank_app/src/components/Button.jsx
@@ -1,11 +1,10 @@
 import React from 'react'
+import styles from '../style'
 
-function Button() {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+const Button = () => (
+    <button type='button' className={`py-4 px-6 bg-discount-gradient fon-poppins font-medium text-[18px] text-white outline-none rounded-[10px]`}>
+      Get Started
+    </button>
+)
 
 export default Button
